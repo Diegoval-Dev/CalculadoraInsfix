f2f03f2517e5874b5c8073b21d81bd2e25a883b9
Diego Pablo Valenzuela Palacios <diegopablo012@gmail.com>
2023-02-17 12:48:58 -0600

    Add files via upload

---
+++ src/Factroy.java
@@ -0,0 +1,19 @@
+import structure.*;
+import structure5.AbstractList;
+
+public class Factroy {
+    public void getInstance(String wichStack){
+        switch (wichStack){
+            case "ArrayList":
+                break;
+            case "Vector":
+                break;
+            case "SimpleList":
+                break;
+            case "DoubleList":
+                break;
+            default:
+                break;
+        }
+    }
+}
